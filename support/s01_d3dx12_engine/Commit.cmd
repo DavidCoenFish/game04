1253ed55f312e7547e5d91b7bdf8d7a78936e42a
DavidCoenFish <fish_dsc@yahoo.co.uk>
2022-07-23 16:30:58 +1000

    Automatron auto generated commit message 2022-07-23T16:30:57

--- support/s01_d3dx12_engine/Commit.cmd
+++ support/s01_d3dx12_engine/Commit.cmd
@@ -5,20 +5,27 @@ cls
 
 echo %time% %~nx0 %~1
 
-echo "%~dp0..\..\build\s01_d3dx12_engine\Automatron03\Release\bin\net5.0\Automatron03.exe"
-echo "%~dp0..\..\build\s01_d3dx12_engine\Automatron03\Release\bin\net5.0\Automatron03.dll"
+SET AUTOMOTRON_EXE="%~dp0..\..\build\s01_d3dx12_engine\Automatron03\Release\bin\net5.0\Automatron03.exe"
+SET AUTOMOTRON_DLL="%~dp0..\..\build\s01_d3dx12_engine\Automatron03\Release\bin\net5.0\Automatron03.dll"
+SET DEVENV="C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\devenv.com"
+
+::msbuild -t:restore
+
+echo %AUTOMOTRON_EXE%
+echo %AUTOMOTRON_DLL%
+echo %DEVENV%
 
 SET MISSING_AUTOMATRON=
-IF NOT EXIST "%~dp0..\..\build\s01_d3dx12_engine\Automatron03\Release\bin\net5.0\Automatron03.exe" SET MISSING_AUTOMATRON=TRUE
-IF NOT EXIST "%~dp0..\..\build\s01_d3dx12_engine\Automatron03\Release\bin\net5.0\Automatron03.dll" SET MISSING_AUTOMATRON=TRUE
+IF NOT EXIST %AUTOMOTRON_EXE% SET MISSING_AUTOMATRON=TRUE
+IF NOT EXIST %AUTOMOTRON_DLL% SET MISSING_AUTOMATRON=TRUE
 
 IF defined MISSING_AUTOMATRON (
-   echo "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\devenv.com" "%~dp0s01_d3dx12_engine.sln" /build "Release|x64" /project Automatron03
-   call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\devenv.com" "%~dp0s01_d3dx12_engine.sln" /build "Release|x64" /project Automatron03
+   echo %DEVENV% "%~dp0s01_d3dx12_engine.sln" /build "Release|x64" /project Automatron03
+   call %DEVENV% "%~dp0s01_d3dx12_engine.sln" /build "Release|x64" /project Automatron03
    )
 
-echo "%~dp0..\..\build\s01_d3dx12_engine\Automatron03\Debug\bin\netcoreapp3.1\Automatron03.exe" %~dp0..\..\ %~dp0Automatron03\TasksCommitOnly
-call "%~dp0..\..\build\s01_d3dx12_engine\Automatron03\Debug\bin\netcoreapp3.1\Automatron03.exe" %~dp0..\..\ %~dp0Automatron03\TasksCommitOnly
+echo %AUTOMOTRON_EXE% %~dp0..\..\ %~dp0Automatron03\TasksCommitOnly
+call %AUTOMOTRON_EXE% %~dp0..\..\ %~dp0Automatron03\TasksCommitOnly
 if %errorlevel% NEQ 0 (
    echo Automatron03.exe exited with %errorlevel%
    exit /b %errorlevel%
