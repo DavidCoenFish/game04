16b9bf5a79f5adc8e1e279396a46aabb2231bd26
DavidCoenFish <fish_dsc@yahoo.co.uk>
2022-07-23 15:40:12 +1000

    Automatron auto generated commit message 2022-07-23T15:40:08

--- support/s01_d3dx12_engine/Commit.cmd
+++ support/s01_d3dx12_engine/Commit.cmd
@@ -5,12 +5,12 @@ cls
 
 echo %time% %~nx0 %~1
 
-echo "%~dp0..\..\build\Automatron03\Release\bin\net5.0\Automatron03.exe"
-echo "%~dp0..\..\build\Automatron03\Release\bin\net5.0\Automatron03.dll"
+echo "%~dp0..\..\build\s01_d3dx12_engine\Automatron03\Release\bin\net5.0\Automatron03.exe"
+echo "%~dp0..\..\build\s01_d3dx12_engine\Automatron03\Release\bin\net5.0\Automatron03.dll"
 
 SET MISSING_AUTOMATRON=
-IF NOT EXIST "%~dp0..\..\build\Automatron03\Release\bin\net5.0\Automatron03.exe" SET MISSING_AUTOMATRON=TRUE
-IF NOT EXIST "%~dp0..\..\build\Automatron03\Release\bin\net5.0\Automatron03.dll" SET MISSING_AUTOMATRON=TRUE
+IF NOT EXIST "%~dp0..\..\build\s01_d3dx12_engine\Automatron03\Release\bin\net5.0\Automatron03.exe" SET MISSING_AUTOMATRON=TRUE
+IF NOT EXIST "%~dp0..\..\build\s01_d3dx12_engine\Automatron03\Release\bin\net5.0\Automatron03.dll" SET MISSING_AUTOMATRON=TRUE
 
 IF defined MISSING_AUTOMATRON (
    echo "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\devenv.com" "%~dp0s01_d3dx12_engine.sln" /build "Release|x64" /project Automatron03
