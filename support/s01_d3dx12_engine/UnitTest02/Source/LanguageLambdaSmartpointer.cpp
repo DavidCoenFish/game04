166aa2296dc3a30851b19d67b08d6600715821e7
DavidCoenFish <fish_dsc@yahoo.co.uk>
2022-09-03 22:06:44 +1000

    Automatron auto generated commit message 2022-09-03T22:02:49

---
+++ support/s01_d3dx12_engine/UnitTest02/Source/LanguageLambdaSmartpointer.cpp
@@ -0,0 +1,44 @@
+#include "UnitTestPCH.h"
+
+namespace LanguageLambdaSmartpointer
+{
+	std::atomic_int s_flag;
+
+	class Scope
+	{
+	public:
+		Scope()
+		{
+			s_flag = 1;
+		}
+		~Scope()
+		{
+			s_flag = 2;
+		}
+	};
+
+	static void SimpleThread(std::shared_ptr<Scope> pScope)
+	{
+		std::this_thread::sleep_for(std::chrono::milliseconds(100));
+		Microsoft::VisualStudio::CppUnitTestFramework::Assert::AreEqual(1, s_flag.load());
+		pScope = nullptr;
+		Microsoft::VisualStudio::CppUnitTestFramework::Assert::AreEqual(2, s_flag.load());
+	}
+
+	TEST_CLASS(Basic)
+	{
+	public: 
+		TEST_METHOD(LambdaTakingSmartpointerOwnership)
+		{
+			s_flag = 0;
+			std::thread thread;
+			{
+				auto pScope = std::make_shared< Scope >();
+				thread = std::thread(SimpleThread, pScope);
+				Microsoft::VisualStudio::CppUnitTestFramework::Assert::AreEqual(1, s_flag.load());
+			}
+			thread.join();
+			Microsoft::VisualStudio::CppUnitTestFramework::Assert::AreEqual(2, s_flag.load());
+		}
+	};
+}
